420e2805d4e8ba9d55633abea98e3b4c73943f41
Sebastián Fretes <sebafretesca@gmail.com>
2024-06-10 15:32:05 -0400

    header & guitar components and db created

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,34 @@
-import { useState } from 'react'
-// import './App.css'
+import { useEffect, useState } from 'react'
+import { Header } from './components/Header';
+import { Guitar } from './components/Guitar';
+import { db } from './data/db';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    setData(db);
+  }, [])
 
   return (
     <>
-      <h1>Guitar Music</h1>
+    <Header />
+
+    <div className="container-xl mt-5">
+        <h2 className="text-center">Our Collection</h2>
+
+        <div className="row mt-5">
+          <Guitar />        
+        </div>
+    </div>
+
+
+    <footer className="bg-dark mt-5 py-5">
+        <div className="container-xl">
+            <p className="text-white text-center fs-4 mt-4 m-md-0">Guitar Music - All rights reserved.</p>
+        </div>
+    </footer>
     </>
   )
 }
