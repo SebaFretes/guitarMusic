138064bbc913bd5e0150ec40069dc964de469bc8
Sebastián Fretes <sebafretesca@gmail.com>
2024-06-10 18:00:15 -0400

    each guitar was mapped

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,29 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import { Header } from './components/Header';
 import { Guitar } from './components/Guitar';
 import { db } from './data/db';
 
-
 function App() {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    setData(db);
-  }, [])
+  const [data, setData] = useState(db);
 
   return (
     <>
-    <Header />
+      <Header />
 
-    <div className="container-xl mt-5">
+      <div className="container-xl mt-5">
         <h2 className="text-center">Our Collection</h2>
 
         <div className="row mt-5">
-          <Guitar />        
+          <Guitar />
         </div>
-    </div>
+      </div>
 
 
-    <footer className="bg-dark mt-5 py-5">
+      <footer className="bg-dark mt-5 py-5">
         <div className="container-xl">
-            <p className="text-white text-center fs-4 mt-4 m-md-0">Guitar Music - All rights reserved.</p>
+          <p className="text-white text-center fs-4 mt-4 m-md-0">Guitar Music - All rights reserved.</p>
         </div>
-    </footer>
+      </footer>
     </>
   )
 }
